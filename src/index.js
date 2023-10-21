f86debc5753a335d7d0e0c6b7aba86e1af238704
Sergio Nezhigay <abc1971h@gmail.com>
2023-10-21 13:38:23 +0300

    changed to HashRouter

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
+      {" "}
       <App />
-    </BrowserRouter>
+    </HashRouter>
+    {/* <BrowserRouter> */}
+
+    {/* </BrowserRouter> */}
   </React.StrictMode>
 );
