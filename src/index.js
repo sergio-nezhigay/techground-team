397c1c68d99fa2f110e0471a64d830fdd33cbcf7
Sergio Nezhigay <abc1971h@gmail.com>
2023-10-21 14:04:04 +0300

    add /

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter basename="/techground-team">
+    <HashRouter basename="/techground-team/">
       <App />
     </HashRouter>
     {/* <BrowserRouter> */}
